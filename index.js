d9125fcdf5a98a41778234f42fd4fbcaadcbf205
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-10-22 01:09:40 +0500

    d

--- index.js
+++ index.js
@@ -2,66 +2,87 @@ const fs=require('fs');
 const data=JSON.parse(fs.readFileSync('data.json','utf-8'));
 const index=fs.readFileSync('index.html','utf-8');
 const errorPage=fs.readFileSync('error.html','utf-8');
+const products=data;
 
 //make server with express
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
-//Third party  midleware
 
 server.use(morgan('default'))
+server.use(express.static('public'));
+server.use(express.json())
 
-//Midle ware to see and check info like eventsx in solidity loger
-// server.use((req,res,next)=>{
-//     console.log(req.method,req.ip,req.hostname,new Date(),req.get('User-Agent'));
-//     next();
-// });//It is application level middleWare
-//++++++++++++++++++Most Imp middleware++++++++++++++++
-//server.use(express.static('public'));
-//It mean we have folder name public in which files some files which are showing
 
-// third party
-//this midleware use to read body
-//server.use(express.json())
-//These are rout middle wear
-let auth=((req,res,next)=>{
-    // console.log(req.query.password);
-    // if(req.bosy.password==="123"){
-    //     next();
-    // }else{
-    //     res.sendStatus(401); // 401 unauthorised status
-    // }
-    next()
+//API -Endpoint -Roite
+
+//Product
+
+// API ROOT=>product
+//Base URL, google.com/api/vs OR localhost etc
+
+//Read GET/Product ==> can call by two method
+
+  //First complete
+server.get('/products',(req,res)=>{
+    res.json(data);
+}); 
+
+
+
+
+//CRUD operation
+
+// 1):-Create
+
+server.post('/products',(req,res)=>{
+    console.log(req.body);
+    data.push(req.body);
+    res.status(201).json({type:'POST'});
 });
-let auth2=((req,res,next)=>{
-    console.log(req.query.password);
-    if(req.query.password==="1234"){
-        next();
-    }else{
-        res.sendStatus(401); // 401 unauthorised status
-    }
+
+
+
+
+// 2):-Read
+////Second by makeing route of individual product
+server.get('/products/:id',(req,res)=>{
+    const id = +req.params.id;
+    console.log("these :"+req.params.id);
+    //const product=data.find(p=>p.id===id)
+    const product=data.find(p=>p.id===id)
+    res.status(200).json(product);
 });
-server.get('/product/:id',auth,(req,res)=>{
-    res.json({type:'GET'});
+
+// 3):- Update 
+//There are two methods for update data
+//i) put
+//In put we override data
+ server.put('/products/:id',(req,res)=>{
+    const id = +req.params.id;
+    const productIndex = data.findIndex(p=>p.id===id);
+    data.splice(productIndex,1,{...req.body,id:id});
+    res.status(201).json()
+ })
+//ii)Patch ==> most recommended
+// in patch we only modify the individual properties
+server.patch('/products/:id',(req,res)=>{
+    const id = +req.params.id;
+    const productIndex = data.findIndex(p=>p.id===id);
+    const singleProduct=data[productIndex]
+    data.splice(productIndex,1,{...singleProduct,...req.body});
+    res.status(201).json()
 });
-server.post('/',auth2,(req,res)=>{
-    res.json({type:'POST'});
-});
+
+
+
+
 server.delete('/',(req,res)=>{
     res.json({type:'DELETE'});
 });
-server.patch('/',(req,res)=>{
-    res.json({type:'PATCH'});
-});
+
 server.get('/demo',(req,res)=>
 {
-    //res.sendStatus(404);
-    //res.json(data);
     res.send("Server start....")
-    //res.sendFile('C:/Users/Dell/OneDrive/Desktop/Expressjs/index.html');
-    //we can send also stras
-    //res.status(200).send("Server start....")
 });
 server.listen(8080)
-
-//https://github.com/coderdost/full-stack-dev-2023#more-info
